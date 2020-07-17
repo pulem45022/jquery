1e1c8b29359d5622524145a5ca90333203324e4f
Damir <pulem45022@Yandex.ru>
2020-07-17 18:39:47 +0400

    money

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 /*jshint esversion: 6 */
 'use strict';
-let a = 4;
-console.log(4/0);
-console.log('string'*7);
-console.log(a);
 
-let persone = [
-    'name: John',
-    'age: 23',
-    'isMarried: false'
-];
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-console.log(persone[0]);
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
