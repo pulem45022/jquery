e5e09645c054c184f8bdc3f679eb159221104a46
Damir <pulem45022@Yandex.ru>
2020-07-17 17:57:18 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+/*jshint esversion: 6 */
+'use strict';
+let a = 4;
+console.log(4/0);
+console.log('string'*7);
+console.log(a);
+
+let persone = [
+    'name: John',
+    'age: 23',
+    'isMarried: false'
+];
+
+console.log(persone[0]);
